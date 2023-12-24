81674ea3e0c057beb3a1d78343aa9898972e5f34
Kajal-7 <kajalkaushal700@gmail.com>
2023-12-24 18:24:36 +0530

    Query Dashboard page added

--- src/components/QueryDashboard.jsx
+++ src/components/QueryDashboard.jsx
@@ -1,15 +1,36 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Grid from '@mui/material/Grid'
+import QueryHistory from './QueryHistory'
+import QueryHandler from './QueryHandler'
 
 /*
 @description
-Query Dashboard Page
+The component renders both query editor and query history section.
+This component also deals with updating the query history list state.
 */
 
 const QueryDashboard = () => {
+  const [list, setNewList] = useState([])
 
   return (
-   <div> Hello</div>
+    <Grid container spacing={2} style={{ padding: '2.2em 0' }}>
+      <Grid
+        item
+        lg={6}
+        xs={12}
+        style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <QueryHandler updateList={setNewList} list={list} />
+      </Grid>
+      <Grid
+        item
+        lg={6}
+        xs={12}
+        style={{ display: 'flex', justifyContent: 'center' }}
+      >
+        <QueryHistory list={list} updateList={setNewList} />
+      </Grid>
+    </Grid>
   )
 }
 
