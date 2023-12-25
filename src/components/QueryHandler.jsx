423652fa8b3038d4d9b0ce762b69867543f6bb10
Kajal-7 <kajalkaushal700@gmail.com>
2023-12-25 10:05:24 +0530

    Added Loader when waiting for query result

--- src/components/QueryHandler.jsx
+++ src/components/QueryHandler.jsx
@@ -1,6 +1,7 @@
 import React, { memo, useCallback, useState } from 'react'
 import QueryResult from './QueryResult'
 import QueryEditor from './QueryEditor'
+import Loader from './ui/Loader'
 
 /*
 @description
@@ -8,6 +9,7 @@ The component fetches the result corresponding to the query from database.
 */
 const QueryHandler = memo(({ updateList, list }) => {
   const [queryResult, setQueryResult] = useState()
+  const [loading, setLoading] = useState(false)
 
   const addItemInList = useCallback(
     (result, query) => {
@@ -26,7 +28,7 @@ const QueryHandler = memo(({ updateList, list }) => {
   )
 
   const fetchQueryResult = useCallback(async (query) => {  
-   
+    setLoading(true)
     const response = await fetch(' https://sql-editor.onrender.com/api/query',{
       method: 'POST',
       body: JSON.stringify({query}),
@@ -35,6 +37,7 @@ const QueryHandler = memo(({ updateList, list }) => {
       }
     })
     const result = await response.json()
+    setLoading(false)
     setQueryResult(result)
     addItemInList(result, query)
   }, [addItemInList])
@@ -42,7 +45,7 @@ const QueryHandler = memo(({ updateList, list }) => {
   return (
     <div style={{ width: '90%' }}>
       <QueryEditor onValidationSuccess={fetchQueryResult} list={list}></QueryEditor>
-      <QueryResult queryResult={queryResult}></QueryResult>
+      {loading ? <Loader/> :  <QueryResult queryResult={queryResult}></QueryResult>}
     </div>
   )
 })
